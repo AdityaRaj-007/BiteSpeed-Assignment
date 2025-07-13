d61f317148e6057d26839a30218c08e5c71dd662
AdityaRaj-007 <iamadityaraj.2001@gmail.com>
2025-07-13 22:46:24 +0530

    Adding SSL

--- src/db/setup.ts
+++ src/db/setup.ts
@@ -5,6 +5,9 @@ import "dotenv/config";
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 export const db = drizzle(pool, { schema });
